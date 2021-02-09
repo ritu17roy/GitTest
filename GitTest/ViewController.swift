ed95edc0ed7d51eec363116dad4607b16e00c1f1
Amrita <amrita@Sushmas-MacBook-Pro.local>
2021-02-09 21:17:00 +0530

    first git commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Amrita on 09/02/21.
+//  Copyright © 2021 Amrita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
